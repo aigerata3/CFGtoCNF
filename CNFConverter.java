d1cc4a3363145e511ea0f74032bfeada756a2681
rgilyard <gilyardrachel@gmail.com>
2023-11-26 13:28:29 -0800

    Add convert to binary function

--- CNFConverter.java
+++ CNFConverter.java
@@ -43,7 +43,51 @@ public class CNFConverter {
 	}
 
 	private void convertToBinary() {
-		// Implementation...
+		// Create new grammar
+		HashMap<String, ArrayList<ArrayList<String>>> newGrammar = new HashMap<>();
+		int suffix = 0;
+
+		// For Variable
+		for (String lhs : grammar.keySet()) {
+			// System.out.println("LHS: " + lhs);
+			//
+			for (ArrayList<String> rhs : grammar.get(lhs)) {
+				// System.out.println("RHS: " + rhs);
+				// If the right hand side is longer than two terms
+				if (rhs.size() > 2) {
+					// Create a new left hand side
+					String newLhs = lhs;
+					while (rhs.size() > 1) {
+						// Make a new variable for the production
+						String newRhsFirstSymbol = rhs.remove(0);
+
+						// Make sure the new variable is unique
+						String newVariable;
+						do {
+							newVariable = newLhs + suffix++;
+						} while (newGrammar.containsKey(newVariable));
+
+						// Create new right hand side list
+						ArrayList<String> newProductionRhs = new ArrayList<>();
+						newProductionRhs.add(newRhsFirstSymbol);
+						newProductionRhs.add(newVariable);
+
+						// Add new production
+						newGrammar.putIfAbsent(newLhs, new ArrayList<>());
+						newGrammar.get(newLhs).add(newProductionRhs);
+
+						newLhs = newVariable;
+					}
+					newGrammar.putIfAbsent(newLhs, new ArrayList<>());
+					newGrammar.get(newLhs).add(rhs);
+				} else {
+					newGrammar.putIfAbsent(lhs, new ArrayList<>());
+					newGrammar.get(lhs).add(rhs);
+				}
+			}
+			// System.out.println();
+		}
+		grammar = newGrammar;
 	}
 	
 	private void convertPairToBinary() {
