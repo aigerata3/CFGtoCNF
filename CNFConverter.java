057f9eda68204c30968575fa4fd68b4332a0d139
rgilyard <gilyardrachel@gmail.com>
2023-11-26 12:12:35 -0800

    Update CNFConverter to work with hashmap

--- CNFConverter.java
+++ CNFConverter.java
@@ -11,7 +11,7 @@ public class CNFConverter {
     }
 
     public static void main(String[] args) {
-        String fileName = "input2.txt";
+        String fileName = "input3.txt";
         CFGHashMapParser parser = new CFGHashMapParser();
         HashMap<String, ArrayList<ArrayList<String>>> grammar = parser.parseFile(fileName);
 
@@ -24,6 +24,7 @@ public class CNFConverter {
         eliminateUnit();
         convertToBinary();
         // save to output txt file
+		printGrammar(this.grammar);
     }
 
     private void eliminateNull() {
@@ -34,7 +35,61 @@ public class CNFConverter {
         // Implementation...
     }
 
+	// If a production's right hand side has more than two symbols, break it up
     private void convertToBinary() {
-        // Implementation...
-    }
+		// Create new grammar
+		HashMap<String, ArrayList<ArrayList<String>>> newGrammar = new HashMap<>();
+		int suffix = 0;
+
+		// For Variable
+		for (String lhs : grammar.keySet()) {
+			System.out.println("LHS: " + lhs);
+			// 
+			for (ArrayList<String> rhs : grammar.get(lhs)) {
+				System.out.println("RHS: " + rhs);
+				// If the right hand side is longer than two terms
+				if (rhs.size() > 2) {
+					// Create a new left hand side
+					String newLhs = lhs;
+					while (rhs.size() > 1) {
+						// Make a new variable for the production
+						String newRhsFirstSymbol = rhs.remove(0);
+
+						// Make sure the new variable is unique
+						String newVariable;
+						do {
+							newVariable = newLhs + suffix++;
+						} while (newGrammar.containsKey(newVariable));
+
+						// Create new right hand side list
+						ArrayList<String> newProductionRhs = new ArrayList<>();
+						newProductionRhs.add(newRhsFirstSymbol);
+						newProductionRhs.add(newVariable);
+
+						// Add new production
+						newGrammar.putIfAbsent(newLhs, new ArrayList<>());
+						newGrammar.get(newLhs).add(newProductionRhs);
+
+						newLhs = newVariable;
+					}
+					newGrammar.putIfAbsent(newLhs, new ArrayList<>());
+					newGrammar.get(newLhs).add(rhs);
+				} else {
+					newGrammar.putIfAbsent(lhs, new ArrayList<>());
+					newGrammar.get(lhs).add(rhs);
+				}
+			}
+			System.out.println();
+		}
+
+		grammar = newGrammar;
+	}
+
+
+	// Prints the hashmap grammar
+	private static void printGrammar(HashMap<String, ArrayList<ArrayList<String>>> grammar) {
+		for (String key : grammar.keySet()) {
+			System.out.println("Key: " + key + ", Value: " + grammar.get(key));
+		}
+	}
 }
