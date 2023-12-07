d7ba2cf5c4451ddb26402ee3b32a9cf75b4ab035
rgilyard <gilyardrachel@gmail.com>
2023-12-07 03:10:09 -0800

    Add Prelim Null Eliminator

--- NullProductionEliminator.java
+++ NullProductionEliminator.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class NullProductionEliminator {
     private HashMap<String, ArrayList<ArrayList<String>>> grammar;
@@ -9,24 +10,142 @@ public class NullProductionEliminator {
     }
 
     // Vicki's Function
-    public HashMap<String, ArrayList<ArrayList<String>>> eliminateNull() {
-        // Implementation...
-        System.out.println("Eliminating nulls...");
-        // A *-> lambda
-        // 1. Find all nullable variables that derive lambda
-        // Initialize a set for nullable variables
-        // Initialize a prev set for variables
-        // Do while variable set and prev variable set are different
-            // Deepcopy variable set to prev variable set
-            // For each lhs (variable) in the grammar
-                // For each rhs of the production for lhs
-                    // If the rhs is all nullable variables
-                        // Add lhs to nullable variables list
+	// Gets all the unit productions for a variable and adds them to a set
+	private void getVarsThatLeadToNull(HashSet<String> nullSet) {
+        // For each variable A in the grammar
+        for (String lhs : grammar.keySet()) {
+            // If that variable A leads to a null variable in the set or lambda
+            for (ArrayList<String> rhs : grammar.get(lhs)) {
+                // If lambda
+                if (rhs.get(0) == "lambda") {
+                    nullSet.add(rhs.get(0));
+                // Or if null variable
+                } else if (rhs.size() == 1 && nullSet.contains(rhs.get(0))) {
+                    nullSet.add(rhs.get(0));
+                }
+            }
+        }
+	}
 
-        // 2. For each production A -> a, construct productions A -> x where x is obtained from 'a'
-        // by removing one or multiple variables from step 1
-
-        // 3. Combine the original productions with the results of step 2 and remove lambda productions
-        return grammar;
+    // Returns true if the production contains a nullable variable
+    private boolean containsNullVariables(ArrayList<String> rhs, HashSet<String> nullSet) {
+        for (String symbol : rhs) {
+            return nullSet.contains(symbol);
+        }
+        return false;
     }
+
+    // Returns list of productions with all combinations of nullables removed
+    private ArrayList<ArrayList<String>> getNullifications(ArrayList<String> rhs, HashSet<String> nullSet) {
+        ArrayList<ArrayList<String>> nullies = new ArrayList<>();
+        // Get the indices of all nullable variables in production
+        // Add set for indices of nullables
+        HashSet<Integer> nullIndices = new HashSet<>();
+        for (int i = 0; i < rhs.size(); i++) {
+            String symbol = rhs.get(i);
+            if (nullSet.contains(symbol)) {
+                // Add index to index set
+                nullIndices.add(i);
+            }
+        }
+
+        // Get all subsets of indices
+        HashSet<HashSet<Integer>> indexSubsets = getAllSubsets(nullIndices);
+
+        // Iterate over each subset of indices
+        for (HashSet<Integer> indexSubset : indexSubsets) {
+            // Create a new list for this subset
+            ArrayList<String> currentNullification = new ArrayList<>();
+            // Iterate over the original rhs list
+            for (int i = 0; i < rhs.size(); i++) {
+                // If the index is not in the current subset, add the element
+                if (!indexSubset.contains(i)) {
+                    currentNullification.add(rhs.get(i));
+                }
+            }
+            // Add the new list to nullies
+            nullies.add(currentNullification);
+        }
+
+        return nullies;
+    }
+
+    // Shamelessly stolen from the innernet
+    private HashSet<HashSet<Integer>> getAllSubsets(HashSet<Integer> originalSet) {
+        HashSet<HashSet<Integer>> allSubsets = new HashSet<>();
+        ArrayList<Integer> elementList = new ArrayList<>(originalSet);
+        int numberOfSubsets = 1 << originalSet.size(); // 2^n subsets
+
+        for (int subsetMask = 0; subsetMask < numberOfSubsets; subsetMask++) {
+            HashSet<Integer> subset = new HashSet<>();
+            for (int i = 0; i < elementList.size(); i++) {
+                // Check if the ith element is in this subset
+                if ((subsetMask & (1 << i)) != 0) {
+                    subset.add(elementList.get(i));
+                }
+            }
+            allSubsets.add(subset);
+        }
+        return allSubsets;
+    }
+
+	// Get rid of all lambda prods
+	public HashMap<String, ArrayList<ArrayList<String>>> eliminateNull() {
+		// Create new grammar
+		HashMap<String, ArrayList<ArrayList<String>>> newGrammar = new HashMap<>();
+
+        // Make a set prev
+        HashSet<String> prevNullProdSet = new HashSet<>();
+        // Make set current
+        HashSet<String> currNullProdSet = new HashSet<>();
+        // do while prev set is different from current set
+        do {
+            System.out.println("In do while.");
+            // Deep copy current set, assign to prev set
+            prevNullProdSet = new HashSet<String>(currNullProdSet);
+            // Add next group of null productions
+            getVarsThatLeadToNull(currNullProdSet);
+        } while (!currNullProdSet.equals(prevNullProdSet));
+
+
+        // For each production
+        for (String lhs : grammar.keySet()) {
+			// Create a new rhs list
+			ArrayList<ArrayList<String>> newRhs = new ArrayList<>();
+			for (ArrayList<String> rhs : grammar.get(lhs)) {
+				// If that production contains null variables
+                if (containsNullVariables(rhs, currNullProdSet)) {
+                    // Add unit production that account for all possible nullifications
+                    ArrayList<ArrayList<String>> nullifications = getNullifications(rhs, currNullProdSet);
+                    // Add nullifications to the new rhs list
+                    newRhs.addAll(nullifications);
+                } else {
+                    newRhs.add(rhs);
+                }
+			}
+			// Add new rhs to the new grammar 
+			newGrammar.put(lhs, newRhs);
+		}
+
+        // Remove all lambdas
+        // For each variable A in the grammar
+        for (String lhs : grammar.keySet()) {
+            // Create a new rhs list
+            ArrayList<ArrayList<String>> newRhs = new ArrayList<>();
+            // For each production from the RHS, add to the set
+            for (ArrayList<String> rhs : grammar.get(lhs)) {
+                // If it is NOT a lambda production
+                if (rhs.get(0) != "lambda") {
+                    // Add production to the new rhs
+                    newRhs.add(rhs);
+                }
+            }
+            // Add new rhs to the new grammar if it's not empty
+            if (newRhs.size() > 0) {
+                newGrammar.put(lhs, newRhs);
+            }
+        }
+
+        return newGrammar;
+	}
 }
--- UnitProductionEliminator.java
+++ UnitProductionEliminator.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.HashSet;
 
 public class UnitProductionEliminator {
@@ -11,7 +10,7 @@ public class UnitProductionEliminator {
     }
 
 	// Finds all the units productions in a set of variables and adds them to the set
-	public void addUnitsToSet(HashSet<String> unitSet) {
+	private void addUnitsToSet(HashSet<String> unitSet) {
 		// For each variable B in the set
 		for (String lhs : unitSet) {
 			// Add any unit productions to the set
@@ -20,7 +19,7 @@ public class UnitProductionEliminator {
 	}
 
 	// Gets all the unit productions for a variable and adds them to a set
-	public void getUnitsFromRhs(String lhs, HashSet<String> unitSet) {
+	private void getUnitsFromRhs(String lhs, HashSet<String> unitSet) {
 		// For each production from the RHS, add to the set
 		for (ArrayList<String> rhs : grammar.get(lhs)) {
 			// If it is a nonterminal
@@ -30,19 +29,8 @@ public class UnitProductionEliminator {
 		}
 	}
 
-	// Removes all unit productions from the rhs of the given variable
-	public void deleteUnits(String lhs) {
-		// For each production from the RHS
-		for (ArrayList<String> rhs : grammar.get(lhs)) {
-			// If the symbol is a variable (nonterminal)
-			if (rhs.size() == 1 && Character.isUpperCase(rhs.get(0).charAt(0))) {
-				// remove rhs from grammar.get(lhs)
-			}
-		}
-	}
-
 	// Returns all the productions for a variable that are not unit productions
-	public ArrayList<ArrayList<String>> getNonUnits(String lhs) {
+	private ArrayList<ArrayList<String>> getNonUnits(String lhs) {
 		ArrayList<ArrayList<String>> nonUnits = new ArrayList<>();
 		// For each production for this variable lhs
 		for (ArrayList<String> rhs : grammar.get(lhs)) {
@@ -56,13 +44,14 @@ public class UnitProductionEliminator {
 	}
 
     // Erin's Functions
-	// Get rid of all unit prods (run the eliminateUnit function until the output doesn't change)
+	// Get rid of all unit prods 
 	public HashMap<String, ArrayList<ArrayList<String>>> eliminateUnit() {
 		// Create new grammar
 		HashMap<String, ArrayList<ArrayList<String>>> newGrammar = new HashMap<>();
 		// Make hashmap of Variable: set of unit production (chains)
 		HashMap<String, HashSet<String>> unitProds = new HashMap<>();
 
+		System.out.println("Making Unit Sets.");
 		// For each variable A in the grammar
 		for (String lhs : grammar.keySet()) {
 			// Make a set prev
@@ -74,17 +63,21 @@ public class UnitProductionEliminator {
 			HashSet<String> currUnitSet = new HashSet<>();
 			// do while prev set is different from current set
 			do {
+				// // Make new current set
+				// currUnitSet = new HashSet<>();
+				System.out.println("In do while.");
 				// Deep copy current set, assign to prev set
 				prevUnitSet = new HashSet<String>(currUnitSet);
 				// Add next group of unit productions
 				addUnitsToSet(currUnitSet);
-			} while (currUnitSet.equals(prevUnitSet));
+			} while (!currUnitSet.equals(prevUnitSet));
 			// Remove original variable A from the set
 			currUnitSet.remove(lhs);
 			// Add set to unit prods hashmap
 			unitProds.put(lhs, currUnitSet);
 		}
 
+		System.out.println("Replacing units with non units.");
 		// THEN
 		ArrayList<ArrayList<String>> nonUnitProds;
 		// For each variable A in the unit prods hashmap
@@ -99,6 +92,7 @@ public class UnitProductionEliminator {
 			}
 		}
 
+		System.out.println("Removing units.");
 		// Remove all unit productions
 		// For each variable A in the grammar
 		for (String lhs : grammar.keySet()) {
@@ -112,92 +106,12 @@ public class UnitProductionEliminator {
 					newRhs.add(rhs);
 				}
 			}
-			// Add new rhs to the new grammar
-			newGrammar.put(lhs, newRhs);
+			// Add new rhs to the new grammar if it's not empty
+			if (newRhs.size() > 0) {
+				newGrammar.put(lhs, newRhs);
+			}
 		}
 
-		// System.out.println("UNITS");
-		// HashMap<String, ArrayList<ArrayList<String>>> previousGrammar;
-		// do {
-		// 	previousGrammar = deepCopy(grammar); // Deep copy the current state of grammar
-		// 	eliminateUnitLevel(); // Apply the unit elimination
-		// 	System.out.println();
-		// 	printGrammar(previousGrammar);
-		// 	printGrammar(grammar);
-		// 	System.out.println();
-		// } while (!grammar.equals(previousGrammar)); // Continue until no change
         return newGrammar;
-	}
-
-	// Creates a deep copy of a grammar for comparison to function output
-	private HashMap<String, ArrayList<ArrayList<String>>> deepCopy(HashMap<String, ArrayList<ArrayList<String>>> original) {
-		// Create copy
-		HashMap<String, ArrayList<ArrayList<String>>> copy = new HashMap<>();
-		// For each variable
-		for (Map.Entry<String, ArrayList<ArrayList<String>>> entry : original.entrySet()) {
-			String key = entry.getKey();
-			// Make a copy of the right hand sides
-			ArrayList<ArrayList<String>> valueCopy = new ArrayList<>();
-
-			for (ArrayList<String> list : entry.getValue()) {
-				// Strings are immutable, so direct copy is fine
-				ArrayList<String> listCopy = new ArrayList<>(list); 
-				valueCopy.add(listCopy);
-			}
-			copy.put(key, valueCopy);
-		}
-		return copy;
-	}
-
-	// Eliminate unit productions
-	private void eliminateUnitLevel() {
-		// New grammar to replace the old one
-		HashMap<String, ArrayList<ArrayList<String>>> newGrammar = new HashMap<>();
-
-		// 1. Find a unit production A -> B
-		// 2. Get the right hand side for the B
-		// 3. Add the right hand side from B to A
-		// 4. Delete A -> B
-
-		// For each variable in the original grammar (We'll call this variable A)
-		for (String lhs : grammar.keySet()) {
-			// Create a new list of productions
-			ArrayList<ArrayList<String>> newProductions = new ArrayList<>();
-			// For each production for this variable 
-			for (ArrayList<String> rhs : grammar.get(lhs)) {
-				// Check for unit production (single uppercase character)
-				if (rhs.size() == 1 && Character.isUpperCase(rhs.get(0).charAt(0))) {
-					// Unit production A -> B found, add B's productions to A
-					// Ger variable as string
-					String rhsVariable = rhs.get(0);
-					// Get all productions for B (rhsVariable)
-					ArrayList<ArrayList<String>> rhsProductions = grammar.getOrDefault(rhsVariable,
-							new ArrayList<ArrayList<String>>());
-
-					// Add all productions of B to A's new productions if not already present
-					for (ArrayList<String> prod : rhsProductions) {
-						if (!newProductions.contains(prod)) {
-							newProductions.add(prod);
-						}
-					}		
-					// Add all productions of B to A's new productions
-					// newProductions.addAll(rhsProductions);
-				} else {
-					// Keep the original non-unit production
-					newProductions.add(rhs);
-				}
-			}
-			// Update the new grammar with the new productions for A
-			newGrammar.put(lhs, newProductions);
-		}
-		// Replace the old grammar with the new one
-		grammar = newGrammar;
-	}
-
-	private static void printGrammar(HashMap<String, ArrayList<ArrayList<String>>> grammar) {
-		System.out.println("Parsed Grammar:");
-		for (Map.Entry<String, ArrayList<ArrayList<String>>> entry : grammar.entrySet()) {
-			System.out.println(entry.getKey() + " -> " + entry.getValue());
-		}
 	}
 }
